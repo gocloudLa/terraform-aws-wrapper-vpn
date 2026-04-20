fa20fc840bcfd9588a1da327a496cab8a931dd5a
Federico Midaglia <fmidaglia@gocloud.la>
2026-04-20 01:36:24 -0300

    chore: update code style & documentation

--- data_sources.tf
+++ data_sources.tf
@@ -1,9 +1,2 @@
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
-
-/*----------------------------------------------------------------------*/
-/* Network | datasources                                                */
-/*----------------------------------------------------------------------*/
-data "aws_availability_zones" "available" {
-  state = "available"
-}
